a323e4514931e5d97d6ee72c45b4d3f8659f3929
unknown <geormerla@gmail.com>
2020-12-17 12:10:11 -0500

    base code to start application

---
+++ src/main/java/com/gina/budget/BudgetApplication.java
@@ -0,0 +1,21 @@
+package com.gina.budget;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class BudgetApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/gina/budget/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.gina.budget;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BudgetApplication.class);
+	}
+
+}
